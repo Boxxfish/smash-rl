19f83b735d7520015b4499bd468f813ea4d4cae3
Ben Giacalone <bensgiacalone@gmail.com>
2023-05-26 16:07:44 -0400

    Integrate rapier

--- smash_rl_rust/src/micro_fighter_env.rs
+++ smash_rl_rust/src/micro_fighter_env.rs
@@ -1,5 +1,22 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 use pyo3::prelude::*;
+
+/// Stores all the plugins needed for human mode.
+pub struct HumanPlugin;
+
+impl Plugin for HumanPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Micro Fighter".into(),
+                resolution: (400.0, 400.0).into(),
+                ..default()
+            }),
+            ..default()
+        }));
+    }
+}
 
 /// Simple fighting game environment.
 #[pyclass]
@@ -16,7 +33,9 @@ impl MicroFighterEnv {
     pub fn run(&self) {
         App::new()
             .add_plugin(HumanPlugin)
-            .add_system(hello_world)
+            .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+            .add_plugin(RapierDebugRenderPlugin::default())
+            .add_startup_system(setup)
             .run();
     }
 }
@@ -27,15 +46,16 @@ impl Default for MicroFighterEnv {
     }
 }
 
-/// Stores all the plugins needed for human mode.
-pub struct HumanPlugin;
-
-impl Plugin for HumanPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins);
-    }
-}
-
-fn hello_world() {
-    println!("Hello World!");
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Collider::cuboid(500.0, 50.0),
+        TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)),
+    ));
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::ball(50.0),
+        Restitution::coefficient(0.7),
+        TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)),
+    ));
 }
