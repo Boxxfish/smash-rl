c5649bb7f7a0760b42224c30adec3b776309a320
Ben Giacalone <bensgiacalone@gmail.com>
2023-07-21 14:07:20 -0400

    Add visualization to Rust env

--- smash_rl_rust/src/binaries/run_env.rs
+++ smash_rl_rust/src/binaries/run_env.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 use smash_rl_rust::env::MFEnv;
 
 fn main() {
-    let mut env = MFEnv::new(1, 4);
+    let mut env = MFEnv::new(1, 4, true, (0, 2, 3));
     let mut rng = rand::thread_rng();
     loop {
         env.bot_step(rng.gen_range(0..9));
--- smash_rl_rust/src/env.rs
+++ smash_rl_rust/src/env.rs
@@ -1,5 +1,6 @@
 use crate::micro_fighter::{MicroFighter, StepOutput};
 use buffer_graphics_lib::prelude::*;
+use minifb::{Window, WindowOptions};
 use tch::Tensor;
 
 const IMG_SIZE: u32 = 64;
@@ -9,6 +10,11 @@ fn insert_obs(obs: Tensor, frame_stack: &mut [Tensor]) {
         frame_stack[i] = frame_stack[i - 1].copy();
     }
     frame_stack[0] = obs;
+}
+
+struct RenderState {
+    buffer: Vec<u32>,
+    window: Window,
 }
 
 /// Rust version of the Micro Fighter environment.
@@ -25,6 +31,8 @@ pub struct MFEnv {
     max_skip_frames: u32,
     num_frames: u32,
     num_channels: u32,
+    render_state: Option<RenderState>,
+    view_channels: (u32, u32, u32),
 }
 
 pub struct MFEnvInfo {
@@ -32,7 +40,12 @@ pub struct MFEnvInfo {
 }
 
 impl MFEnv {
-    pub fn new(max_skip_frames: u32, num_frames: u32) -> Self {
+    pub fn new(
+        max_skip_frames: u32,
+        num_frames: u32,
+        rendering: bool,
+        view_channels: (u32, u32, u32),
+    ) -> Self {
         let options = (tch::Kind::Float, tch::Device::Cpu);
         let game = MicroFighter::new(false);
         let num_channels = 4;
@@ -66,6 +79,20 @@ impl MFEnv {
                 )
             })
             .collect();
+        let render_state = if rendering {
+            Some(RenderState {
+                buffer: vec![0; (IMG_SIZE * IMG_SIZE) as usize],
+                window: Window::new(
+                    "MFEnv - AI View",
+                    IMG_SIZE as usize,
+                    IMG_SIZE as usize,
+                    WindowOptions::default(),
+                )
+                .expect("Couldn't create window"),
+            })
+        } else {
+            None
+        };
         Self {
             game,
             observation_space,
@@ -79,6 +106,8 @@ impl MFEnv {
             max_skip_frames,
             num_frames,
             num_channels,
+            render_state,
+            view_channels,
         }
     }
 
@@ -244,31 +273,40 @@ impl MFEnv {
                 .collect();
             let polygon = Polygon::from_points(&points);
             graphics.draw_polygon(polygon, DrawType::Fill(Color::rgb(1, 1, 1)));
-            let box_arr = Tensor::from_slice(&buffer.iter().step_by(4).copied().collect::<Vec<u8>>()).reshape([IMG_SIZE as i64, IMG_SIZE as i64]);
+            let box_arr =
+                Tensor::from_slice(&buffer.iter().step_by(4).copied().collect::<Vec<u8>>())
+                    .reshape([IMG_SIZE as i64, IMG_SIZE as i64]);
             let inv_box_arr = 1.0 - &box_arr;
             hit_channel = hit_channel * &inv_box_arr + hbox.is_hit as u8 as f32 * &box_arr;
             dmg_channel = dmg_channel * &inv_box_arr + (hbox.damage as f32 / 10.0) * &box_arr;
-            player_channel = player_channel * &inv_box_arr + (hbox.is_player == is_player) as u8 as f32 * &box_arr;
+            player_channel = player_channel * &inv_box_arr
+                + (hbox.is_player == is_player) as u8 as f32 * &box_arr;
             box_channel = box_channel * &inv_box_arr + &box_arr;
         }
 
         vec![hit_channel, dmg_channel, player_channel, box_channel]
     }
 
-    pub fn render(&self) {
-        // if self.render_mode == "human":
-        //     channels = self.player_frame_stack[0]
-        //     r = channels[self.view_channels[0]]
-        //     g = channels[self.view_channels[1]]
-        //     b = channels[self.view_channels[2]]
-        //     view = np.flip(np.stack([r, g, b]).transpose(2, 1, 0), 1).clip(0, 1) * 255.0
-        //     view_surf = pygame.Surface([IMG_SIZE, IMG_SIZE])
-        //     pygame.surfarray.blit_array(view_surf, view)
-        //     pygame.transform.scale(
-        //         view_surf, [IMG_SIZE * IMG_SCALE, IMG_SIZE * IMG_SCALE], self.screen
-        //     )
-        //     pygame.display.flip()
-        //     self.clock.tick(60)
+    pub fn render(&mut self) {
+        if let Some(render_state) = &mut self.render_state {
+            let channels = &self.player_frame_stack[0];
+            let r = channels.get(self.view_channels.0 as i64);
+            let g = channels.get(self.view_channels.1 as i64);
+            let b = channels.get(self.view_channels.2 as i64);
+            let window = &mut render_state.window;
+            for y in 0..IMG_SIZE {
+                for x in 0..IMG_SIZE {
+                    let index = (y * IMG_SIZE + x) as usize;
+                    let r = (r.double_value(&[y as i64, x as i64]) * 255.0) as u32;
+                    let g = (g.double_value(&[y as i64, x as i64]) * 255.0) as u32;
+                    let b = (b.double_value(&[y as i64, x as i64]) * 255.0) as u32;
+                    render_state.buffer[index] = (r << 24) + (g << 16) + (b << 8);
+                }    
+            }
+            window
+                .update_with_buffer(&render_state.buffer, IMG_SIZE as usize, IMG_SIZE as usize)
+                .expect("Couldn't render buffer");
+        }
     }
 
     pub fn bot_obs(&self) -> (Tensor, Tensor) {
